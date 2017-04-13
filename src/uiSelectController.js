721a85b8135baf3aeaa86824e5af92a4e33e48ca
piebev <piebe.de.vries@geodan.nl>
2017-04-13 15:16:00 +0200

    fix the loss of the first character in IE

--- src/uiSelectController.js
+++ src/uiSelectController.js
@@ -166,6 +166,7 @@ uis.controller('uiSelectCtrl',
 
   ctrl.focusSearchInput = function () {
     ctrl.searchInput[0].focus();
+    ctrl.focusser.trigger('blur'); // in all browsers but IE the focus causes a blur, for IEs sake force a blur
     $scope.$broadcast('uis:activate');
   };
 
